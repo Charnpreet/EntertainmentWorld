1404b661c5d70c33721e3d9dcec10ffbff567e9c
Charnpreet <sony_baf@yahoo.co.in>
2020-04-07 15:50:04 +1000

    Intial Setup
    initial setup with tab bar,
    tab bar has 3 items :- dashboard, search and fav
    each item has its own storyboard

---
+++ EntainmentWorld/EntainmentWorld/Controlers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EntainmentWorld
+//
+//  Created by CHARNPREET SINGH on 7/4/20.
+//  Copyright © 2020 CHARNPREET SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
